8a6c3abcd05e0f7b0d5d252df6a9f85b3d589a0e
Yuuki Rika <yuuki.rikachan@gmail.com>
2022-08-20 18:15:56 +0700

    Hello Blue Kernel.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // This function is the entry point, since the linker looks for a function
-    // named `_start` by default.
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+static HELLO: &[u8] = b"Hello Blue Kernel!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
